af4162964b346edeb773aaac27f25930de485753
White Hat Dude <119371214+WhiteH4tDude@users.noreply.github.com>
2026-09-05 00:17:07 -0300

    update version on conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ copyright = f"{datetime.now().year}, {author}"
 try:
     from picklock import __version__ as release
 except Exception:  # pragma: no cover - docs can build without the package installed
-    release = "0.2.2"
+    release = "0.2.3"
 
 version = ".".join(release.split(".")[:2])
 
